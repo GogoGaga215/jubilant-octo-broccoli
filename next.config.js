5261b96c0f5bcdf1ffaeac8eaf58b3a49f9386f3
GogoGaga215 <carlokimlusterio112@gmail.com>
2026-06-10 20:50:57 +0800

    Add serverExternalPackages to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  serverExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
 }
 module.exports = nextConfig
